0360762a126f3cb6a76150787e38022a7fdc7ec8
CadenceWu <90173356+CadenceWu@users.noreply.github.com>
2022-08-19 22:37:30 +0800

    Add project files.

---
+++ Car Parking/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Car_Parking
+{
+    partial class CarParking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxPlate = new System.Windows.Forms.ListBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonLinearSearch = new System.Windows.Forms.Button();
+            this.buttonBinarySearch = new System.Windows.Forms.Button();
+            this.buttonTag = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.tooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxPlate
+            // 
+            this.listBoxPlate.FormattingEnabled = true;
+            this.listBoxPlate.ItemHeight = 20;
+            this.listBoxPlate.Location = new System.Drawing.Point(38, 38);
+            this.listBoxPlate.Name = "listBoxPlate";
+            this.listBoxPlate.Size = new System.Drawing.Size(178, 444);
+            this.listBoxPlate.TabIndex = 0;
+            this.tooltip.SetToolTip(this.listBoxPlate, "The rego plate data list");
+            this.listBoxPlate.SelectedIndexChanged += new System.EventHandler(this.listBoxPlate_SelectedIndexChanged);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(225, 78);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(126, 26);
+            this.textBoxInput.TabIndex = 1;
+            this.tooltip.SetToolTip(this.textBoxInput, "Enter the rego plate");
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(399, 78);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(117, 45);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Open";
+            this.tooltip.SetToolTip(this.buttonOpen, "Select and open the data from pre-saved text files");
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(236, 394);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(98, 45);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.tooltip.SetToolTip(this.buttonSave, "Save the data to the text file");
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(236, 122);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(98, 37);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "Add";
+            this.tooltip.SetToolTip(this.buttonAdd, "Add a rego plate to the list box");
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(236, 165);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(98, 43);
+            this.buttonEdit.TabIndex = 6;
+            this.buttonEdit.Text = "Edit";
+            this.tooltip.SetToolTip(this.buttonEdit, "Select a rego plate from the list box, replace it with the rego plate in the text" +
+        " box");
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(236, 214);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(98, 51);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Delete";
+            this.tooltip.SetToolTip(this.buttonDelete, "Select a rego plate from the list box and remove it");
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonLinearSearch
+            // 
+            this.buttonLinearSearch.Location = new System.Drawing.Point(374, 220);
+            this.buttonLinearSearch.Name = "buttonLinearSearch";
+            this.buttonLinearSearch.Size = new System.Drawing.Size(154, 38);
+            this.buttonLinearSearch.TabIndex = 8;
+            this.buttonLinearSearch.Text = "Linear Search";
+            this.tooltip.SetToolTip(this.buttonLinearSearch, "Type the plate into the text box using linear search ");
+            this.buttonLinearSearch.UseVisualStyleBackColor = true;
+            this.buttonLinearSearch.Click += new System.EventHandler(this.buttonLinearSearch_Click);
+            // 
+            // buttonBinarySearch
+            // 
+            this.buttonBinarySearch.Location = new System.Drawing.Point(374, 280);
+            this.buttonBinarySearch.Name = "buttonBinarySearch";
+            this.buttonBinarySearch.Size = new System.Drawing.Size(154, 40);
+            this.buttonBinarySearch.TabIndex = 9;
+            this.buttonBinarySearch.Text = "Binary Search";
+            this.tooltip.SetToolTip(this.buttonBinarySearch, "Type the plate into the text box using binary search");
+            this.buttonBinarySearch.UseVisualStyleBackColor = true;
+            this.buttonBinarySearch.Click += new System.EventHandler(this.buttonBinarySearch_Click);
+            // 
+            // buttonTag
+            // 
+            this.buttonTag.Location = new System.Drawing.Point(236, 271);
+            this.buttonTag.Name = "buttonTag";
+            this.buttonTag.Size = new System.Drawing.Size(98, 49);
+            this.buttonTag.TabIndex = 10;
+            this.buttonTag.Text = "Tag";
+            this.tooltip.SetToolTip(this.buttonTag, "Select a rego plate from the list box and mark it");
+            this.buttonTag.UseVisualStyleBackColor = true;
+            this.buttonTag.Click += new System.EventHandler(this.buttonTag_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Vehicle Plate Details";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(374, 394);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(142, 45);
+            this.buttonReset.TabIndex = 12;
+            this.buttonReset.Text = "Reset";
+            this.tooltip.SetToolTip(this.buttonReset, "Clear all the data items from the list");
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 21);
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 514);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Padding = new System.Windows.Forms.Padding(2, 0, 14, 0);
+            this.StatusStrip.Size = new System.Drawing.Size(596, 28);
+            this.StatusStrip.TabIndex = 13;
+            // 
+            // CarParking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 542);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTag);
+            this.Controls.Add(this.buttonBinarySearch);
+            this.Controls.Add(this.buttonLinearSearch);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.listBoxPlate);
+            this.Name = "CarParking";
+            this.Text = "CarParking";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CarParking_FormClosed);
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxPlate;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonLinearSearch;
+        private System.Windows.Forms.Button buttonBinarySearch;
+        private System.Windows.Forms.Button buttonTag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.ToolTip tooltip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+    }
+}
+
---
+++ Car Parking/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace Car_Parking
+{
+    
+    public partial class CarParking : Form
+    {
+        //Create a list RegoPlate
+        //Q2 A list<> data structure of data type "string
+        List<string> RegoPlate = new List<string>();
+        string currentFileName = "";
+       
+        //Bindingsourece is an intermediate link between the list and the list box, it encapsulate the data source for a form 
+        BindingSource bs = new BindingSource();
+
+        public CarParking()
+        {
+            InitializeComponent();
+            bs.DataSource = RegoPlate; //Intermidate binding source variable with RegoPlate data source
+        }
+        private bool ValidName(string checkThisName)
+        {
+            //Not allow duplicated plate to exist in the list box
+            if (RegoPlate.Exists(duplicate => duplicate.Equals(checkThisName)))
+                return false;
+            else
+                return true;
+        }
+
+        //Q13 Tag: Create a tag method
+        private void Tag()
+        {
+           //Create a tag method wih a prefix "Z" added in front
+            if (listBoxPlate.SelectedIndex != -1)
+            {
+                RegoPlate[listBoxPlate.SelectedIndex] = "Z: " + listBoxPlate.SelectedItem;
+            }
+        }
+        //Q13 Tag: When a rego plate is selected from the list box and "tagged", an additional character value
+        //"Z" will be prefixed to the rego plate.
+        private void buttonTag_Click(object sender, EventArgs e)
+        {
+           //Create a string which is the item selected from the list box
+            string selectedText = (string)listBoxPlate.SelectedItem; 
+
+           //If the selected item started with the prefix "Z", remove the prefix
+            if (selectedText.StartsWith("Z") && (listBoxPlate.SelectedIndex >= 0))
+           {
+               RegoPlate.Add(selectedText.TrimStart('Z',':',' '));
+               RegoPlate.ToList();
+               RegoPlate.RemoveAt(listBoxPlate.SelectedIndex);
+               RegoPlate.Remove(textBoxInput.Text);
+               toolStripStatusLabel1.Text = ("Untagged");
+           }
+           //Import the tag method to tag the selected item in the list box
+           else 
+           {
+               Tag();
+               toolStripStatusLabel1.Text = "Tagged";
+           }
+           //Q9 Display and Sort: Sorted automatically in the list box by using built-in list sort method
+            RegoPlate.Sort();
+            textBoxInput.Clear();
+            textBoxInput.Focus();
+            //Tell the list box to update using the current version of rego plate list
+            listBoxPlate.DataSource = bs;
+            //Not changing string type to other type. Using string in the update >> false
+            //Call ResetBindings to update the textboxes, only values have changed
+            bs.ResetBindings(false);
+        }
+        //Q10 Binary Search: Using binary search to search for the plate in the list
+        private void buttonBinarySearch_Click(object sender, EventArgs e)
+        {
+            //Using built-in BinarySearch method to find the target entered in the text box
+            if (!string.IsNullOrWhiteSpace(textBoxInput.Text))
+            {
+               RegoPlate.Sort();
+               if (RegoPlate.BinarySearch(textBoxInput.Text) >= 0)
+                   toolStripStatusLabel1.Text = ("Found");
+               else
+                    toolStripStatusLabel1.Text = ("Not found");
+                    textBoxInput.Clear();
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = ("Please enter a plate");
+            }
+            textBoxInput.Clear();
+            textBoxInput.Focus();
+        }
+        //Q11 Linear Search: Using linear search to search for the plate in the list
+        private void buttonLinearSearch_Click(object sender, EventArgs e)
+        {
+            string target= textBoxInput.Text;
+            bool found = false;
+
+            //Using linear search to find the target
+            if (!string.IsNullOrWhiteSpace(textBoxInput.Text))
+            {
+                for (int x = 0; x < listBoxPlate.Items.Count; x++)
+                {
+                    if (listBoxPlate.Items.Contains(target))
+                    {
+                      toolStripStatusLabel1.Text = ("Found");
+                      found = true;
+                      break;
+                    }
+                }
+                if (!found)
+                {
+                    toolStripStatusLabel1.Text = ("Not found");
+                }
+                textBoxInput.Clear();
+                textBoxInput.Focus();
+            }
+             else
+             {
+                toolStripStatusLabel1.Text = ("Please enter a plate");
+                textBoxInput.Clear();
+                textBoxInput.Focus();
+            }
+        }
+
+        //Q3 Open: Load the data into the list<>.
+        //When the open button is clicked, the user can select different data from pre-saved text files.
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            //Displays a standard dislog box that prompts the user to open a file.
+            string textFileName;
+            OpenFileDialog openTextFile = new OpenFileDialog();//Creat an OpenFileDialog control object
+            openTextFile.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            //Restrict the file type
+            openTextFile.Filter = "txt flies (*.txt)|*.txt";
+            DialogResult sr = openTextFile.ShowDialog();//ShowDialog method displays the OpenFileDialog
+
+            if (sr == DialogResult.OK)
+            {
+                //Read the content of the file
+                textFileName = openTextFile.FileName;
+                //Set the current file name as the open file name
+                currentFileName = textFileName;
+                toolStripStatusLabel1.Text = ("File opened");
+            }
+            else
+            {
+                return;
+            }
+            try
+            {
+                RegoPlate.Clear();
+                using (StreamReader reader = new StreamReader(File.OpenRead(textFileName)))
+                {
+                    //If it is not end of stream, keep reading the text file
+                    while (!reader.EndOfStream)
+                    {
+                        RegoPlate.Add(reader.ReadLine());//Read one line a time
+                        listBoxPlate.DataSource = bs;
+                        bs.ResetBindings(false);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                toolStripStatusLabel1.Text = ("File not openned");
+            }
+        }
+        //Q4 Enter: To add a rego plate to the list<>
+        //The user will type the data value into the TextBox and click the Add button
+        //The data will be added to the list<> and the TextBox will be cleared.
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //Set up the rego plate pattern as number(1-9) + three capital characters+ "-" + three characters in a row
+            string pattern = @"^[0-9][A-Z][A-Z][A-Z]-\d{3}$";
+
+            if (string.IsNullOrWhiteSpace(textBoxInput.Text))
+            {
+                toolStripStatusLabel1.Text = ("Please enter a plate");
+            }
+            else if (!ValidName(textBoxInput.Text))
+            {
+                toolStripStatusLabel1.Text = ("The plate is already existed");
+            }
+            else if (Regex.IsMatch(textBoxInput.Text,pattern)==false)//The rego plate that is entered to the text box has to match the pattern
+            {
+                toolStripStatusLabel1.Text = ("Please enter a valid plate");
+            }
+            else
+            {
+                RegoPlate.Add(textBoxInput.Text);
+                toolStripStatusLabel1.Text="Successful";
+            }
+            listBoxPlate.DataSource = bs;
+            RegoPlate.Sort();
+            bs.ResetBindings(false);
+            textBoxInput.Clear();
+            textBoxInput.Focus();
+           
+            
+        }
+        //Q5 Leave: To remove rego plate
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(textBoxInput.Text))
+            {
+                toolStripStatusLabel1.Text = ("Please select or enter a plate");
+            }
+            //When the plate is entering to the list box, using binary search to search for the plate
+            else if (RegoPlate.BinarySearch(textBoxInput.Text) < 0)
+            {
+
+               toolStripStatusLabel1.Text = ("The plate cannot be found");
+            }
+            else
+            {
+                listBoxPlate.SetSelected(listBoxPlate.SelectedIndex, true);
+                //Remove the item in the text box
+                RegoPlate.Remove(textBoxInput.Text);
+                toolStripStatusLabel1.Text = "Successful";
+            }
+            listBoxPlate.DataSource = bs;
+            bs.ResetBindings(false);
+            textBoxInput.Clear();
+            textBoxInput.Focus();
+        }
+        //Q12 Close and Save: Save all the data back to a text file
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //Create a SaveFileDialog class
+            SaveFileDialog saveTextFileDialoge = new SaveFileDialog();
+            saveTextFileDialoge.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            //Restrict the file type as "txt" file type only
+            saveTextFileDialoge.Filter = "txt flies (*.txt)|*.txt";
+            DialogResult sr = saveTextFileDialoge.ShowDialog();
+
+            if (sr == DialogResult.OK)
+            {
+                //Use the SaveTextFile method to write the data into the saving file
+                SaveTextFile(saveTextFileDialoge.FileName);
+            }
+            if (sr == DialogResult.Cancel)
+            {
+                return;
+            }
+        }
+        //Q12 Create a save method
+        private void SaveTextFile(string filename)
+        {
+           //Create a save method 
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filename, false))
+                {
+                    foreach (var plate in RegoPlate)
+                    {
+                        writer.WriteLine(plate);
+                        listBoxPlate.DataSource = bs;
+                        bs.ResetBindings(false);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("File not saved");
+            }
+        }
+        //Q12 Close and Save: A form closing method using the save method so all the data from list<> will
+        // written back to the list<>
+        private void CarParking_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                // Validate file name and increment the file number after closing it
+                if (listBoxPlate.Items != null)
+                {
+                    //Remove the first 5 characters of the current file name
+                    int num = int.Parse(Path.GetFileNameWithoutExtension(currentFileName).Remove(0, 5));
+                    num++;
+                    string newvalue;
+                    if (num <= 9)
+                        newvalue = "0" + num.ToString();
+                    else
+                        newvalue = num.ToString();
+                        string newFileName = ("demo_" + newvalue + ".txt");
+                        SaveTextFile(newFileName);
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+        //Q6 Edit: Alter the information from the text box to the list box selected plate
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+
+            string pattern = @"^[0-9][A-Z][A-Z][A-Z]-\d{3}$";
+            if (string.IsNullOrWhiteSpace(textBoxInput.Text))
+            {
+                textBoxInput.Focus();
+                toolStripStatusLabel1.Text = ("Please enter a plate");
+            }
+            else if (!ValidName(textBoxInput.Text))
+            {
+
+                textBoxInput.Focus();
+                toolStripStatusLabel1.Text = ("The plate is already existed");
+            }
+            else if (Regex.IsMatch(textBoxInput.Text, pattern) == false)
+            {
+                textBoxInput.Focus();
+                toolStripStatusLabel1.Text = ("This is not a valid plate");
+            }
+            else
+            {
+                //Replace the plate in the text box with the selected item
+                RegoPlate[listBoxPlate.SelectedIndex] = textBoxInput.Text;
+                RegoPlate.Sort();
+                toolStripStatusLabel1.Text = ("Successful");
+            }
+            listBoxPlate.DataSource = bs;
+            bs.ResetBindings(false);
+            textBoxInput.Clear();
+            textBoxInput.Focus();
+        }
+        //Q7 Reset: Clear all the data items from the list<>
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+           //Clear all the data in the rego plate list box
+            RegoPlate.Clear();
+            bs.ResetBindings(false);
+            textBoxInput.Clear();
+            toolStripStatusLabel1.Text = ("Reset");
+
+
+        }
+        //Q8 Single Data Display: Create a single click method
+        //When a data item is selected from the list box on the left, the information is displayed
+        //in the text box on the right
+        private void listBoxPlate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Show the list box selected item into the text box
+            if (listBoxPlate.SelectedItem != null)
+            {
+                textBoxInput.Text = listBoxPlate.SelectedItem.ToString();
+            }
+        }
+
+    }
+}
